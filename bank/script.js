713b6575e09b2d318653b0c0cf49e5dd1008bf84
Ayon <ayonjodder177@gmail.com>
2022-02-11 02:35:19 +0600

    Error handler added

--- bank/script.js
+++ bank/script.js
@@ -1,29 +1,58 @@
-//Handling Deposit and Balance section by Add Balance button
-document.getElementById('add-balance').addEventListener('click', function () {
-    //For adding balance to Deposit section
-    const inputAmount = parseFloat(document.getElementById('input-amount').value);
-    const depositAmount = parseFloat(document.getElementById('deposit-amount').innerText);
-    const totalDeposit = depositAmount + inputAmount;
-    document.getElementById('deposit-amount').innerText = totalDeposit;
-
-    //For adding balance to Balance section
-    const balanceAmount = parseFloat(document.getElementById('balance-amount').innerText);
-    const totalBalance = balanceAmount + inputAmount;
-    document.getElementById('balance-amount').innerText = totalBalance;
-
-    //Clearing input field after adding balance
-    document.getElementById('input-amount').value = '';
-});
-//Handling Withdrawn and Balance section by Withdrawn button
-document.getElementById('withraw-balance').addEventListener('click', function () {
-    const withdrawn = parseFloat(document.getElementById('add-withdraw').value);
-    const withdrawAmount = parseFloat(document.getElementById('withdraw-amount').innerText);
-    const totalWithdraw = withdrawn + withdrawAmount;
-    document.getElementById('withdraw-amount').innerText = totalWithdraw;
-
-    const balanceAmount = parseFloat(document.getElementById('balance-amount').innerText);
-    const balanceSoFar = balanceAmount - withdrawn;
-    document.getElementById('balance-amount').innerText = balanceSoFar;
-
-    document.getElementById('add-withdraw').value = '';
-})
+//Making function for getting value from input field
+function getInputValue(inputId) {
+    const getInput = document.getElementById(inputId);
+    const inputValue = parseFloat(getInput.value);
+    //Clear input filed
+    getInput.value = "";
+    return inputValue;
+  }
+  
+  //Making function for adding balance in deposit and withdraw field
+  function totalCash(depositFieldId, inputAmount) {
+    const depositField = document.getElementById(depositFieldId);
+    const depositPrevious = parseFloat(depositField.innerText);
+    depositField.innerText = depositPrevious + inputAmount;
+  }
+  
+  //Making function for getting the current balance of Total deposit and Total balance field
+  function currentBalance() {
+    const balance = document.getElementById("balance-amount");
+    const totalBalance = parseFloat(balance.innerText);
+    return totalBalance;
+  }
+  
+  //Making function for adding and removing balance from Total balance field
+  function totalBalance(inputAmount, isAdd) {
+    const oldBalance = currentBalance();
+    const balanceTotal = document.getElementById("balance-amount");
+    if (isAdd == true) {
+      balanceTotal.innerText = oldBalance + inputAmount;
+    } else {
+      balanceTotal.innerText = oldBalance - inputAmount;
+    }
+  }
+  
+  //For Add Balance button
+  document.getElementById("add-balance").addEventListener("click", function () {
+    const depositAmount = getInputValue("input-amount");
+    if (depositAmount <= 0) {
+      alert("Please enter a valid amount");
+    } else {
+      totalCash("deposit-amount", depositAmount);
+      totalBalance(depositAmount, true);
+    }
+  });
+  
+  //For Withdraw button
+  document
+    .getElementById("withraw-balance")
+    .addEventListener("click", function () {
+      const withdrawAmount = getInputValue("add-withdraw");
+      if (withdrawAmount <= 0) {
+        alert("Please enter a valid amount");
+      } else {
+        totalCash("withdraw-amount", withdrawAmount);
+        totalBalance(withdrawAmount, false);
+      }
+    });
+  
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //Make a event handler for login button
-document.getElementById('login-button').addEventListener('click', function () {
-    const loginMail = document.getElementById('input-email');
-    const loginPassword = document.getElementById('input-password');
+document.getElementById("login-button").addEventListener("click", function () {
+  const loginMail = document.getElementById("input-email");
+  const loginPassword = document.getElementById("input-password");
 
-    //Redirect the login page to bank page using this mail and pass
-    if (loginMail.value === 'admin' && loginPassword.value === 'admin') {
-        window.location.href = 'bank/index.html'
-    };
+  //Redirect the login page to bank page using this mail and pass
+  if (loginMail.value === "admin" && loginPassword.value === "admin") {
+    window.location.href = "bank/index.html";
+  } else {
+    alert("Invalid Username or Password");
+  }
 });
